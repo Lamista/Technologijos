1ac1021d9b79de8558c6c9212bce77ba3b8d8cf8
Laura <laura.stasiule@gmail.com>
2020-11-23 15:27:35 +0200

    pvz kaip naudojamas jsx

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-React.createClass = createReactClass;
-var HelloComponent = React.createClass({
-    render: function () {
-        return React.createElement(
-            "div",
-            null,
-            "Hello ",
-            this.props.name
-        );
-    }
-});
-ReactDOM.render(React.createElement(
-    HelloComponent,
-    { name: "Jane" }),
-    document.getElementById('root')
-);
+<MyButton color="blue" shadowSize={2}>
+    Click Me
+</MyButton>
+
+React.createElement(
+    MyButton,
+    {
+        color: 'blue'
+        , shadowSize: 2
+    },
+    'Click Me'
+)
+
+    < Komponentas atributas1 = { 2+3 } atributas2 = { someVar } atributas3 = {{ a: 'b' }}/>
