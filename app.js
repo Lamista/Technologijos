729353a76ed132558417d630c895e8da6a4097d7
Laura <laura.stasiule@gmail.com>
2020-11-23 15:30:05 +0200

    Daugiau pvz: JSX vs JSX su ES6

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-<MyButton color="blue" shadowSize={2}>
-    Click Me
-</MyButton>
+//JSX
+React.createClass = createReactClass;
+var HelloComponent = React.createClass({
+    render: function () {
+        return <div>Hello {this.props.name}</div>;
+    }
+});
+ReactDOM.render(React.createElement(
+    HelloComponent,
+    { name: "Jane" }),
+    document.getElementById('root')
+);
 
-React.createElement(
-    MyButton,
-    {
-        color: 'blue'
-        , shadowSize: 2
-    },
-    'Click Me'
-)
-
-    < Komponentas atributas1 = { 2+3 } atributas2 = { someVar } atributas3 = {{ a: 'b' }}/>
+//JSX ir ES6
+class HelloComponent extends React.Component {
+    render() {
+        return <div>Hello {this.props.name}</div>;
+    }
+}
+ReactDOM.render(
+    <HelloComponent name="Jane" />,
+    document.getElementById('root')
+);
